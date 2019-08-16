36b3b39c22c3b8fbeda25c40b8c09fde96c297c8
lukasz19944 <lukasz19944@gmail.com>
2019-08-16 23:19:38 +0200

    Szkielet aplikacji. Closes #1

---
+++ src/main/java/pl/slusarski/top10listappbackend/Top10ListAppBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.slusarski.top10listappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Top10ListAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Top10ListAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/slusarski/top10listappbackend/Top10ListAppBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.slusarski.top10listappbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Top10ListAppBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
